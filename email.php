d5f86c932a8a8999d6893b0f803ff3feb3b9d49a
CristianLegu <cristian.glg94@gmail.com>
2017-06-18 23:46:42 -0500

    Correo
    Archivos de correo

--- email.php
+++ email.php
@@ -1,14 +1,37 @@
 <?php
 
+//Reseteamos variables a 0.
+   $nombre = $email = $subject = $mensaje = $para = $headers = $msjCorreo = NULL;
 
+      //Obtenemos valores input formulario
+      $nombre = $_POST['name'];
+      $email = $_POST['email'];
+      $subject = "Contacto Página Web";   
+      $mensaje = $_POST['msg'];
+      $para = 'ghportonconsuelo@gmail.com';
 
-//$to = "ghportonconsuelo@gmail.com";
-$to = "cristian.glg94@gmail.com";
-$subject = "Nuevo Mensaje de ".$_POST('name');
-$message = $_POST('msg');
-$headers = "From: administracion@hopped.com.mx" ;
- 
-mail($to, $subject, $message, $headers);
+      //Creamos cabecera.
+      $headers = 'From' . " " . "administracion@hopped.com" . "\r\n";
 
+      //Componemos cuerpo correo.
+      $msjCorreo = "Nombre: " . $nombre;
+      $msjCorreo .= "\r\n";
+      $msjCorreo .= "Email: " . $email;
+      $msjCorreo .= "\r\n";
+      $msjCorreo .= "Asunto: " . $subject;
+      $msjCorreo .= "\r\n";
+      $msjCorreo .= "Mensaje: " . $mensaje;
+      $msjCorreo .= "\r\n";
 
+    if (mail($para, $subject, $msjCorreo, $headers)) {
+        echo "<script language='javascript'>
+            alert('Mensaje enviado, muchas gracias, pronto nos pondremos en contacto con usted.');
+            window.location.href='http://granhotelportondeconsuelo.com/';
+         </script>";
+         
+    } else {
+         echo "<script language='javascript'>
+            alert('fallado');
+         </script>";
+    }
 ?>
